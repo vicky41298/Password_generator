c88de1561a32cc842a120ae374709fb6ade841e6
Vignesh S <vicky41298@gmail.com>
2023-05-22 18:50:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,42 +7,63 @@ function generatePasswords() {
 
     var characterSets = document.getElementsByName("character-set");
     var characterList = "";
+    var includesDigits = false;
+    var includesLetters = false;
+    var includesSpecialCharacters = false;
 
     for (var i = 0; i < characterSets.length; i++) {
         if (characterSets[i].checked) {
             var characterSet = characterSets[i].value;
 
-            if (characterSet === "digits")
+            if (characterSet === "digits") {
                 characterList += "0123456789";
-            else if (characterSet === "letters")
+                includesDigits = true;
+            } else if (characterSet === "letters") {
                 characterList += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            else if (characterSet === "special-characters")
-                characterList += "@#$%!";
+                includesLetters = true;
+            } else if (characterSet === "special-characters") {
+                characterList += "!@#$%&*";
+                includesSpecialCharacters = true;
+            }
         }
-    }
-
-    var passwords = [];
-    for (var j = 0; j < count; j++) {
-        var password = "";
-        for (var k = 0; k < length; k++) {
-            var randomIndex = Math.floor(Math.random() * characterList.length);
-            password += characterList.charAt(randomIndex);
-        }
-        passwords.push(password);
     }
 
     var generatedPasswords = document.getElementById("generated-passwords");
-    generatedPasswords.innerHTML = "";
+    generatedPasswords.innerHTML = ""; // Clear previous passwords
 
     for (var j = 0; j < count; j++) {
         var password = "";
-        for (var k = 0; k < length; k++) {
+        var hasDigit = false;
+        var hasLowerCase = false;
+        var hasUpperCase = false;
+        var hasSpecialChar = false;
+
+        while (password.length < length) {
             var randomIndex = Math.floor(Math.random() * characterList.length);
-            password += characterList.charAt(randomIndex);
+            var randomChar = characterList.charAt(randomIndex);
+
+            // Check if character type requirements are met
+            if (includesDigits && !hasDigit && /[0-9]/.test(randomChar)) {
+                hasDigit = true;
+            } else if (includesLetters && !hasLowerCase && /[a-z]/.test(randomChar)) {
+                hasLowerCase = true;
+            } else if (includesLetters && !hasUpperCase && /[A-Z]/.test(randomChar)) {
+                hasUpperCase = true;
+            } else if (includesSpecialCharacters && !hasSpecialChar && /[^a-zA-Z0-9]/.test(randomChar)) {
+                hasSpecialChar = true;
+            }
+
+            password += randomChar;
         }
 
-        var passwordElement = document.createElement("p");
-        passwordElement.textContent = password;
-        generatedPasswords.appendChild(passwordElement);
+        // Check if all character type requirements are met
+        if (includesDigits && includesLetters && includesSpecialCharacters &&
+            hasDigit && hasLowerCase && hasUpperCase && hasSpecialChar) {
+            var passwordElement = document.createElement("p");
+            passwordElement.textContent = password;
+            generatedPasswords.appendChild(passwordElement);
+        } else {
+            j--; // Regenerate password if requirements are not met
+        }
     }
 }
